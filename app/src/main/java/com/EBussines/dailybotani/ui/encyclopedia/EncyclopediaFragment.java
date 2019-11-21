610f662f67d2fb78f0ce4c64fbafcdbdb3c9a62e
navalinovian <viansatria57@gmail.com>
2019-11-21 09:46:42 +0700

    Initial commit

---
+++ app/src/main/java/com/EBussines/dailybotani/ui/encyclopedia/EncyclopediaFragment.java
@@ -0,0 +1,35 @@
+package com.EBussines.dailybotani.ui.encyclopedia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.EBussines.dailybotani.R;
+
+public class EncyclopediaFragment extends Fragment {
+
+    private EncyclopediaViewModel encyclopediaViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        encyclopediaViewModel =
+                ViewModelProviders.of(this).get(EncyclopediaViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_encyclopedia, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        encyclopediaViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/EBussines/dailybotani/ui/encyclopedia/EncyclopediaViewModel.java
@@ -0,0 +1,19 @@
+package com.EBussines.dailybotani.ui.encyclopedia;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EncyclopediaViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public EncyclopediaViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
